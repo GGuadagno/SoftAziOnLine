f85a12ec3e144aac07b014446f3f2688076b823b
GGuadagno <160673822+GGuadagno@users.noreply.github.com>
2024-02-26 15:02:22 +0100

    OK CAUS E MAG 2

--- SoftAziOnLine/Login.aspx.vb
+++ SoftAziOnLine/Login.aspx.vb
@@ -278,7 +278,7 @@ Partial Public Class Login
         '"09/08/2022 16.00 Controllo obbligo N° Telefono per singolo DDT in spedizione"
         'lblRelease.Text = "Release 10/10/2022 15.05 Modifica Gestione Spedizione file DDT: Aggiunta la Profondita / Modifica campo Località senza la Pr. e EUR "
         '+ Format(Now, "dddd d MMMM yyyy, HH:mm:ss") & _
-        lblRelease.Text = "Release 26/02/2024 Corretto documenti/Causale Magazzino: movimenti tra Magazzini, non deve cambiare la Causale 2 e Magazzino 2 in aggiornamento" +
+        lblRelease.Text = "Release 26/02/2024 Corretto documenti/Causale Magazzino: movimenti tra Magazzini, non deve cambiare la Causale 2 e Magazzino 2 in aggiornamento"
         lblRelease.ToolTip = " - 20/02/2024 Modifica Nuova stampa Elenco DDT per Magazzino/Causale: Aggiunto Dal/Al Magazzino quando è un Trasferimento" +
         " - 15/02/2024 Gestione documenti/Causale Magazzino: movimenti tra Magazzini, abilitato oltre alla 2 Causale anche il 2 Magazzino" +
         " - Corretto Gestione ricerca Cliente da gestionale esterno: la Combo clienti non veniva caricata correttamente" +
